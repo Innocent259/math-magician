6475815545760d54538e5c60230cb88ce3b18599
innocent259 <ndahimanainnocent842@gmail.com>
2023-07-06 16:43:17 +0200

    Render quotes in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './Components/Calculator';
+import Quote from './Components/Quote';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quote />
     </div>
   );
 }
