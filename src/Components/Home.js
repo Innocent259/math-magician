cf8a194ddc4a4940eb0371a3ed272e3926c95c37
innocent259 <ndahimanainnocent842@gmail.com>
2023-07-12 13:56:59 +0200

    Create Home component

---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Welcome to our page</h3>
+      <p>
+        Math magiciansis a website for all fans of mathematics.
+        It is a Single Page
+        App (SPA) that allows users to
+        Make simple calculations and Read a random
+        math-related quote.
+      </p>
+    </div>
+  );
+}
